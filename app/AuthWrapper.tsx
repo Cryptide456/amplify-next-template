2db71c3712f447f2f6d0e1db6b62fe8774273b5a
ndias3 <ndias3@gmu.edu>
2025-05-29 12:29:14 -0400

    Added Authenticator

---
+++ app/AuthWrapper.tsx
@@ -0,0 +1,13 @@
+// app/components/AuthWrapper.tsx
+"use client";
+
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+export default function AuthWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Authenticator>{children}</Authenticator>;
+}
